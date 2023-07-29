15a8711e66b3aa9abaefbaa288779bd9b1f484a8
K. Neupane <126298173+kncloudproject@users.noreply.github.com>
2023-07-29 09:35:10 -0500

    Add files via upload

---
+++ 3 - Shell Scripting Hands On Project/Shell Scripting Project.md
@@ -0,0 +1,19 @@
+# Project 5: Shell Scripting Hands-on (Aux project)
+## Shell Scripting Syntax Elements 
+
+1. **Variables :** 
+
+2. **Control Flow :**
+
+3. **Command Substitution :**
+
+4. **Input and Outpu :**
+
+5. **Functions :**
+
+
+## Let's Write the first Shell Script
+
+```
+
+```
---
+++ 3 - Shell Scripting Hands On Project/shell-scripting/shell.sh
@@ -0,0 +1,40 @@
+# Variables
+# name="KISHAN"
+# echo $name
+
+# Control Flow
+
+#!/bin/bash
+
+# Example script to check if a number is positive, negative, or zero
+
+# read -p "Enter a number: " num
+
+# if [ $num -gt 0 ]; then
+#     echo "The number is positive."
+# elif [ $num -lt 0 ]; then
+#     echo "The number is negative."
+# else
+#     echo "The number is zero."
+# fi
+
+
+#!/bin/bash
+
+# Example script to print numbers from 1 to 5 using a for loop
+
+for (( i=1; i<=6; i++ ))
+do
+    echo $i
+done
+
+
+# # Command Substitution
+
+# current_date=`date +%Y-%m-%d`
+
+# current_date=$(date +%Y-%m-%d)
+
+
+
+
